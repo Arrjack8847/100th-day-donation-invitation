272599770fd732134f43be4637c5915b6175383d
Arrjack8847 <smks8847@gmail.com>
2026-09-24 00:23:43 +0800

    Add baby portrait component

---
+++ components/opening/BabyPortrait.tsx
@@ -0,0 +1,11 @@
+export default function BabyPortrait() {
+  return (
+    <div className="baby-frame">
+      <div
+        className="baby-photo"
+        role="img"
+        aria-label="Child portrait"
+      />
+    </div>
+  );
+}
